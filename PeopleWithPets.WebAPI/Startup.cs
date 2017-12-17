18f4d63d09d88d3dd93b4ad9b4caac225d7898b0
Prashant Dhavale <pndhavale@outlook.com>
2017-12-17 16:36:09 +1100

    Setting port as 5000

--- PeopleWithPets.WebAPI/Startup.cs
+++ PeopleWithPets.WebAPI/Startup.cs
@@ -55,6 +55,7 @@ namespace PeopleWithPets.WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            
             app.UseCors("AllowSpecificOrigin");
             app.UseMvc();
         }
